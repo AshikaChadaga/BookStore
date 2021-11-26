2bf65396e9311858d3ea570301ee254a8bb3b226
AshikaChadaga <ashikachadaga@gmail.com>
2021-11-26 19:46:54 +0530

    [Ashika]ADDED: Cart page

---
+++ src/pages/cartpage/CartPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import MyCart from '../../components/mycart/MyCart';
+import Header from '../../components/header/Header';
+import Footer from '../../components/footer/Footer';
+
+function CartPage() {
+    return (
+        <div>
+            <Header />
+            <MyCart />
+            <Footer />
+        </div>
+    )
+}
+
+export default CartPage
