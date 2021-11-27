3524e134ef5a70e3a4de5da14e86e01ceed5496b
AshikaChadaga <ashikachadaga@gmail.com>
2021-11-27 14:25:50 +0530

    [Ashika]ADDED: Authentication by checking id in local Storage

---
+++ src/components/Authentication/Authentication.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class Authentication {
+    constructor(props) {
+        if (localStorage.getItem("id"))
+            this.authenticated = true;
+        else
+            this.authenticated = false;
+
+    }
+    login(callback) {
+        if (localStorage.getItem("id")){
+            this.authenticated = true;
+            callback();
+        }
+
+    }
+    logout() {
+        this.authenticated = false;
+    }
+    isAuthenticated() {
+        return this.authenticated;
+    }
+}
+
+export default new Authentication();
+
