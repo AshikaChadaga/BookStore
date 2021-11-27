d303a6adc70663914b10b5c7e7b3c579cd37f398
AshikaChadaga <ashikachadaga@gmail.com>
2021-11-27 14:18:02 +0530

    [Ashika]REFACTORED: Protected Routing for dashoard and cart

--- src/App.js
+++ src/App.js
@@ -3,22 +3,26 @@ import Homepage from './pages/homepage/Homepage';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/dashboard/Dashboard';
 import CartPage from './pages/cartpage/CartPage';
-// import { Switch } from 'react-router';
+import { ProtectedRoute } from './components/protectedroute/ProtectedRoute';
 
 function App() {
   return (
     <div className="App">
       <Router>
-      <div className="App">
-        <Routes>
-          <Route path='/' element={<Homepage/>}></Route>
-          <Route path='/login' element={<Homepage/>}></Route>
-          <Route path='/dashboard' element={<Dashboard/>}></Route>
-          <Route path='/cart' element={<CartPage/>}></Route>
-          <Route exact path='*' component={() => "404 Not Found"}></Route>
-        </Routes>
-      </div>
-    </Router>
+          <div className="App">
+            <Routes>
+              <Route path='/' element={<Homepage />}></Route>
+              <Route path='/login' element={<Homepage />}></Route>
+              <Route exact path='/dashboard' element={<ProtectedRoute />}>
+                <Route exact path='/dashboard' element={<Dashboard />} />
+              </Route>
+              <Route exact path='/cart' element={<ProtectedRoute />}>
+                <Route exact path='/cart' element={<CartPage />} />
+              </Route>
+              <Route exact path='*' component={() => "404 Not Found"}></Route>
+            </Routes>
+          </div>
+      </Router>
     </div>
   );
 }
