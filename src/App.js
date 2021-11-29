717decfe77b2ed8c0c1b1923e0ee62d059566898
AshikaChadaga <ashikachadaga@gmail.com>
2021-11-29 19:25:52 +0530

    [Ashika]REFACTORED: Removed routing to search page

--- src/App.js
+++ src/App.js
@@ -9,19 +9,19 @@ function App() {
   return (
     <div className="App">
       <Router>
-          <div className="App">
-            <Routes>
-              <Route path='/' element={<Homepage />}></Route>
-              <Route path='/login' element={<Homepage />}></Route>
-              <Route exact path='/dashboard' element={<ProtectedRoute />}>
-                <Route exact path='/dashboard' element={<Dashboard />} />
-              </Route>
-              <Route exact path='/cart' element={<ProtectedRoute />}>
-                <Route exact path='/cart' element={<CartPage />} />
-              </Route>
-              <Route exact path='*' component={() => "404 Not Found"}></Route>
-            </Routes>
-          </div>
+        <div className="App">
+          <Routes>
+            <Route path='/' element={<Homepage />}></Route>
+            <Route path='/login' element={<Homepage />}></Route>
+            <Route exact path='/dashboard' element={<ProtectedRoute />}>
+              <Route exact path='/dashboard' element={<Dashboard />} />
+            </Route>
+            <Route exact path='/cart' element={<ProtectedRoute />}>
+              <Route exact path='/cart' element={<CartPage />} />
+            </Route>
+            <Route exact path='*' component={() => "404 Not Found"}></Route>
+          </Routes>
+        </div>
       </Router>
     </div>
   );
