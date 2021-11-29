c2cf6d26f0777adc80cb80441a4788e4f1b0536b
AshikaChadaga <ashikachadaga@gmail.com>
2021-11-29 19:27:47 +0530

    [Ashika]REFACTORED: Functionality to search

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -4,24 +4,27 @@ import Footer from '../../components/footer/Footer';
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import Avatar from "@mui/material/Avatar";
-import StarIcon from "@mui/icons-material/Star";
-import { green } from "@mui/material/colors";
 import Box from '@mui/material/Box';
 import Pagination from "@mui/material/Pagination";
 import { createTheme, IconButton, PaginationItem } from '@mui/material';
 import { ThemeProvider } from '@emotion/react';
 import ChevronLeftRoundedIcon from '@mui/icons-material/ChevronLeftRounded';
 import ChevronRightRoundedIcon from '@mui/icons-material/ChevronRightRounded';
-import bookImage from "../../assets/dashboard/Image 11.png";
 import { useDispatch, useSelector } from 'react-redux';
 import UserService from '../../service/UserService';
 import './Dashboard.scss';
 import { getCartItems, getWishlistItems } from '../../store/actions/cartAction';
+import DisplayBooks from '../../components/displaybooks/DisplayBooks';
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Avatar from "@mui/material/Avatar";
+import StarIcon from "@mui/icons-material/Star";
+import { green } from "@mui/material/colors";
+import bookImage from "../../assets/dashboard/Image 11.png";
+import Button from "@mui/material/Button";
+import { display } from '@mui/system';
+
 
 const userService = new UserService();
 
@@ -52,6 +55,7 @@ function Dashboard() {
   const dispatch = useDispatch();
   const cartItems = useSelector(state => state.cartItems);
   const wishlistItems = useSelector(state => state.wishlistItems);
+  const [searchWord, setSearchWord] = useState("");
 
   // console.log("Cart Items ", cartItems);
 
@@ -191,6 +195,58 @@ function Dashboard() {
     });
   }
 
+  const displayBooks = (currentBooks) => {
+    return (
+      <div className="display-cards">
+        {
+          currentBooks.map((book) => (
+            <div className="display-books">
+              <Card
+                id={book._id}
+                sx={{ maxWidth: 320, maxHeight: 500, background: "#FFFFFF 0% 0% no-repeat padding-box", border: "1px solid #E2E2E2", borderRadius: "3px", opacity: "1" }}>
+                <CardContent style={{ backgroundColor: "#F5F5F5" }}>
+                  <img width="40%" src={bookImage} alt="book Image" />
+                </CardContent>
+                <CardContent >
+                  <Typography style={{ paddingLeft: "0.7vw", fontWeight: "bold" }} variant="body2" color="black" textAlign="left">
+                    {book.bookName}
+                  </Typography>
+                  <Typography style={{ paddingLeft: "0.7vw", paddingBottom: "1vh" }} variant="body2" color="text.secondary" textAlign="left">
+                    by {book.author}
+                  </Typography>
+                  <Typography style={{ paddingLeft: "0.7vw", fontWeight: "bold", paddingBottom: "1vh" }} variant="body2" color="black" textAlign="left">
+                    Rs. {book.price}
+                  </Typography>
+                  <Typography style={{ paddingLeft: "0.7vw", display: "grid", gridTemplateColumns: "1fr 50fr", textAlign: "left" }} variant="body2" color="black">
+                    <Avatar
+                      sx={{
+                        textAlign: "left",
+                        bgcolor: green[700],
+                        width: 60,
+                        height: 30,
+                        font: "normal normal bold 18px/13px Roboto"
+                      }}
+                      variant="rounded"
+                    >
+                      4.5
+                      <StarIcon sx={{ m: 0.4, width: 18 }} />
+                    </Avatar>
+                    <span style={{ paddingLeft: "10px", paddingTop: "5px" }}>({book.quantity})</span>
+
+                  </Typography>
+                </CardContent>
+                <div >
+                  {generateButtons(book)}
+
+                </div>
+              </Card>
+            </div>
+          ))
+        }
+      </div>
+    )
+  }
+
   useEffect(() => {
     getCartBooks();
     getWishListBooks();
@@ -205,7 +261,7 @@ function Dashboard() {
 
   return (
     <div>
-      <Header />
+      <Header setSearchWord={setSearchWord} />
       <div className="main-content">
         <div className="title">
           <div className="left-content">
@@ -231,71 +287,28 @@ function Dashboard() {
             </FormControl>
           </div>
         </div>
-        <div className="display-cards">
-          {
-            currentBooks.map((book) => (
-              <div className="display-books">
-                <Card
-                  id={book._id}
-                  sx={{ maxWidth: 320, maxHeight: 500, background: "#FFFFFF 0% 0% no-repeat padding-box", border: "1px solid #E2E2E2", borderRadius: "3px", opacity: "1" }}>
-                  <CardContent style={{ backgroundColor: "#F5F5F5" }}>
-                    <img width="40%" src={bookImage} alt="book Image" />
-                  </CardContent>
-                  <CardContent >
-                    <Typography style={{ paddingLeft: "0.7vw", fontWeight: "bold" }} variant="body2" color="black" textAlign="left">
-                      {book.bookName}
-                    </Typography>
-                    <Typography style={{ paddingLeft: "0.7vw", paddingBottom: "1vh" }} variant="body2" color="text.secondary" textAlign="left">
-                      by {book.author}
-                    </Typography>
-                    <Typography style={{ paddingLeft: "0.7vw", fontWeight: "bold", paddingBottom: "1vh" }} variant="body2" color="black" textAlign="left">
-                      Rs. {book.price}
-                    </Typography>
-                    <Typography style={{ paddingLeft: "0.7vw", display: "grid", gridTemplateColumns: "1fr 50fr", textAlign: "left" }} variant="body2" color="black">
-                      <Avatar
-                        sx={{
-                          textAlign: "left",
-                          bgcolor: green[700],
-                          width: 60,
-                          height: 30,
-                          font: "normal normal bold 18px/13px Roboto"
-                        }}
-                        variant="rounded"
-                      >
-                        4.5
-                        <StarIcon sx={{ m: 0.4, width: 18 }} />
-                      </Avatar>
-                      <span style={{ paddingLeft: "10px", paddingTop: "5px" }}>({book.quantity})</span>
 
-                    </Typography>
-                  </CardContent>
-                  <div >
-                    {generateButtons(book)}
-
-                  </div>
-                </Card>
-              </div>
-            ))
-          }
+        <div>
+          {searchWord.length == 0 ? displayBooks(currentBooks) : displayBooks(currentBooks.filter(book => (book.bookName.toLowerCase().includes(searchWord) || book.author.toLowerCase().includes(searchWord))))}
         </div>
-        <ThemeProvider theme={theme}>
-          <Box sx={{ display: 'flex', justifyContent: "center", margin: "25px" }} >
-            <Pagination
-              onChange={handlePageChange}
-              count={Math.ceil(books.length / 12)}
-              color="myColor"
-              page={currentPage}
-              shape="rounded"
-              renderItem={(item) => (
-                <PaginationItem
-                  components={{ previous: left, next: right }}
-                  {...item}
-                />
-              )}
-            />
-          </Box>
-        </ThemeProvider>
       </div>
+      <ThemeProvider theme={theme}>
+        <Box sx={{ display: 'flex', justifyContent: "center", margin: "25px" }} >
+          <Pagination
+            onChange={handlePageChange}
+            count={Math.ceil(books.length / booksPerPage)}
+            color="myColor"
+            page={currentPage}
+            shape="rounded"
+            renderItem={(item) => (
+              <PaginationItem
+                components={{ previous: left, next: right }}
+                {...item}
+              />
+            )}
+          />
+        </Box>
+      </ThemeProvider>
       <Footer />
     </div>
   )
