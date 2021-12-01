647fac0f83c257cfc353d806a8340cbb0d3ba4da
AshikaChadaga <ashikachadaga@gmail.com>
2021-12-01 10:56:14 +0530

    [Ashika]REFACTORED: Snackbar to alert signup success and failure

--- src/pages/signup/SignUp.jsx
+++ src/pages/signup/SignUp.jsx
@@ -10,11 +10,27 @@ import VisibilityOff from "@mui/icons-material/VisibilityOff";
 import Button from "@mui/material/Button";
 import { useForm, Controller } from 'react-hook-form';
 import UserService from '../../service/UserService';
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
 const userService = new UserService();
 function SignUp() {
     //password visibility
     const [passwordVisibility, setPasswordVisibility] = useState(false);
+    const [open, setOpen] = useState(false);
+    const [alertMessage, setAlertMessage ] = useState("");
+    const [messageColor, setMessageColor] = useState("");
+
+    const handleClose = (event, reason) => {
+        if (reason === "clickaway") {
+            return;
+        }
+        setOpen(false);
+    };
     const handlePasswordToggle = () => {
         setPasswordVisibility(!passwordVisibility)
     }
@@ -23,13 +39,19 @@ function SignUp() {
     const onSubmit = data => {
         console.log(data);
         userService.registration("/registration", data)
-        .then((res) => {
-            console.log("User Registered Successfully!!");
-            localStorage.setItem("fullName", res.data.result.fullName);
-        })
-        .catch((error) => {
-            console.log(error);
-        })
+            .then((res) => {
+                console.log("User Registered Successfully!!");
+                localStorage.setItem("fullName", res.data.result.fullName);
+                setAlertMessage("Registered Sucessfully !");
+                setMessageColor("success");
+                setOpen(true);
+            })
+            .catch((error) => {
+                console.log(error);
+                setAlertMessage("User Not Registered !");
+                setMessageColor("error");
+                setOpen(true);
+            })
     }
 
 
@@ -46,7 +68,7 @@ function SignUp() {
                             fullWidth
                             label="Full Name"
                             variant="outlined"
-                            style={{  fontSize: "1px" }}
+                            style={{ fontSize: "1px" }}
                             value={value}
                             onChange={onChange}
                             error={!!error}
@@ -66,7 +88,7 @@ function SignUp() {
                             fullWidth
                             label="Email Id"
                             variant="outlined"
-                            style={{  fontSize: "1px" }}
+                            style={{ fontSize: "1px" }}
                             value={value}
                             onChange={onChange}
                             error={!!error}
@@ -92,7 +114,7 @@ function SignUp() {
                                         onChange={onChange}
                                         error={!!error}
                                         helperText={error ? error.message : " "}
-                                        
+
                                         InputProps={{
                                             endAdornment: (
                                                 <InputAdornment position='end'>
@@ -124,7 +146,7 @@ function SignUp() {
                             fullWidth
                             label="Mobile Number"
                             variant="outlined"
-                            style={{  fontSize: "1px" }}
+                            style={{ fontSize: "1px" }}
                             value={value}
                             onChange={onChange}
                             error={!!error}
@@ -136,6 +158,16 @@ function SignUp() {
                 <Button type="submit" fullWidth variant="contained" style={{ marginTop: "7px", textTransform: "none", background: "#A03037 0% 0% no-repeat padding-box", opacity: 1 }}>
                     Sign Up
                 </Button>
+                <Snackbar
+                    anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+                    open={open}
+                    autoHideDuration={2000}
+                    onClose={handleClose}
+                >
+                    <Alert onClose={handleClose} severity={messageColor} sx={{ width: "100%" }}>
+                        {alertMessage}
+                    </Alert>
+                </Snackbar>
             </form>
         </div>
     )
