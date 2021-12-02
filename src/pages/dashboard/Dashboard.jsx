66d518609251649efead0f9111cb519f8d370457
AshikaChadaga <ashikachadaga@gmail.com>
2021-12-02 18:29:44 +0530

    [Ashika]REFACTORED: Generated book description

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -22,7 +22,11 @@ import StarIcon from "@mui/icons-material/Star";
 import { green } from "@mui/material/colors";
 import bookImage from "../../assets/dashboard/Image 11.png";
 import Button from "@mui/material/Button";
-import loader from '../../assets/dashboard/loader.gif';
+import loader from '../../assets/dashboard/301.gif';
+import Popper from "@mui/material/Popper";
+import Fade from "@mui/material/Fade";
+import Paper from "@mui/material/Paper";
+
 
 
 const userService = new UserService();
@@ -57,6 +61,7 @@ function Dashboard() {
   const [searchWord, setSearchWord] = useState("");
   const [loading, setLoading] = useState(false);
 
+
   // console.log("Cart Items ", cartItems);
 
   const handlePageChange = (event, value) => {
@@ -65,6 +70,17 @@ function Dashboard() {
 
   const handleChange = (event) => {
     setSelection(event.target.value);
+  };
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [open, setOpen] = React.useState(false);
+
+  const handleClick = () => (event) => {
+    setAnchorEl(event.currentTarget);
+    setOpen(true);
+  };
+  const handleLeave = () => (event) => {
+    setOpen(false);
   };
 
   const indexOfLastBook = currentPage * booksPerPage;
@@ -113,13 +129,13 @@ function Dashboard() {
         console.log("Books Displayed!");
         setTimeout(() => {
           setLoading(false);
-        }, 1000);
+        }, 2000);
       })
       .catch(error => {
         console.error('Error encountered while Displaying Books!', error);
         setTimeout(() => {
           setLoading(false);
-        }, 1000);
+        }, 2000);
       });
   }
 
@@ -160,10 +176,15 @@ function Dashboard() {
 
     else {
       return (<div className="button">
-        <Button onClick={() => { addBookToBag(book) }} style={{ backgroundColor: "#A03037", marginLeft: "30px" }} variant="contained">
+        <Button
+          sx={{ display: { font: "normal normal normal 12px/26px Roboto" } }}
+          onClick={() => { addBookToBag(book) }}
+          style={{ backgroundColor: "#A03037", marginLeft: "30px" }}
+          variant="contained">
           ADD TO BAG
         </Button>
         <Button
+          sx={{ display: { font: "normal normal normal 12px/26px Roboto" } }}
           onClick={() => { addBookToWishlist(book) }}
           variant="outlined"
           style={{ color: "black", borderColor: "#878787", marginRight: "30px" }}
@@ -215,37 +236,73 @@ function Dashboard() {
               <Card
                 id={book._id}
                 sx={{ maxWidth: 320, maxHeight: 500, background: "#FFFFFF 0% 0% no-repeat padding-box", border: "1px solid #E2E2E2", borderRadius: "3px", opacity: "1" }}>
-                <CardContent style={{ backgroundColor: "#F5F5F5" }}>
-                  <img width="40%" src={bookImage} alt="book" />
-                </CardContent>
-                <CardContent >
-                  <Typography style={{ paddingLeft: "0.7vw", fontWeight: "bold" }} variant="body2" color="black" textAlign="left">
-                    {book.bookName}
-                  </Typography>
-                  <Typography style={{ paddingLeft: "0.7vw", paddingBottom: "1vh" }} variant="body2" color="text.secondary" textAlign="left">
-                    by {book.author}
-                  </Typography>
-                  <Typography style={{ paddingLeft: "0.7vw", fontWeight: "bold", paddingBottom: "1vh" }} variant="body2" color="black" textAlign="left">
-                    Rs. {book.price}
-                  </Typography>
-                  <Typography style={{ paddingLeft: "0.7vw", display: "grid", gridTemplateColumns: "1fr 50fr", textAlign: "left" }} variant="body2" color="black">
-                    <Avatar
-                      sx={{
-                        textAlign: "left",
-                        bgcolor: green[700],
-                        width: 60,
-                        height: 30,
-                        font: "normal normal bold 18px/13px Roboto"
-                      }}
-                      variant="rounded"
-                    >
-                      4.5
-                      <StarIcon sx={{ m: 0.4, width: 18 }} />
-                    </Avatar>
-                    <span style={{ paddingLeft: "10px", paddingTop: "5px" }}>({book.quantity})</span>
+                <Popper
+                  sx={{ display: { xs: "none", sm: "none" } }}
+                  open={open}
+                  anchorEl={anchorEl}
+                  placement="right-start"
+                  transition
+                  style={{ width: "20%", }}
+                >
+                  {({ TransitionProps }) => (
+                    <Fade {...TransitionProps} timeout={350}>
+                      <Paper>
+                        <Typography sx={{ p: 2 }}>
+                          <h4>Book Detail</h4>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                          enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                          nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                          in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                          nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                          sunt in culpa qui officia deserunt mollit anim id est laborum.
+                          Adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus.
+                          Arcu odio ut sem nulla pharetra diam sit amet nisl. Bibendum
+                          arcu vitae elementum curabitur vitae nunc sed velit dignissim.
+                          Tellus integer feugiat scelerisque varius morbi enim nunc
+                          faucibus a. Leo vel fringilla est ullamcorper eget nulla. Mauris
+                          a diam maecenas sed. Odio eu feugiat pretium nibh. Eu nisl nunc
+                          mi ipsum faucibus vitae aliquet nec. Odio ut sem nulla pharetra
+                          diam sit amet nisl.
+                        </Typography>
+                      </Paper>
+                    </Fade>
+                  )}
+                </Popper>
+                <div onMouseEnter={handleClick()}
+                  onMouseLeave={handleLeave()}>
+                  <CardContent style={{ backgroundColor: "#F5F5F5" }}>
+                    <img width="40%" src={bookImage} alt="book" />
+                  </CardContent>
+                  <CardContent >
+                    <Typography style={{ paddingLeft: "0.7vw", fontWeight: "bold" }} variant="body2" color="black" textAlign="left">
+                      {book.bookName}
+                    </Typography>
+                    <Typography style={{ paddingLeft: "0.7vw", paddingBottom: "1vh" }} variant="body2" color="text.secondary" textAlign="left">
+                      by {book.author}
+                    </Typography>
+                    <Typography style={{ paddingLeft: "0.7vw", fontWeight: "bold", paddingBottom: "1vh" }} variant="body2" color="black" textAlign="left">
+                      Rs. {book.price}
+                    </Typography>
+                    <Typography style={{ paddingLeft: "0.7vw", display: "grid", gridTemplateColumns: "1fr 50fr", textAlign: "left" }} variant="body2" color="black">
+                      <Avatar
+                        sx={{
+                          textAlign: "left",
+                          bgcolor: green[700],
+                          width: 60,
+                          height: 30,
+                          font: "normal normal bold 18px/13px Roboto"
+                        }}
+                        variant="rounded"
+                      >
+                        4.5
+                        <StarIcon sx={{ m: 0.4, width: 18 }} />
+                      </Avatar>
+                      <span style={{ paddingLeft: "10px", paddingTop: "5px" }}>({book.quantity})</span>
 
-                  </Typography>
-                </CardContent>
+                    </Typography>
+                  </CardContent>
+                </div>
                 <div >
                   {generateButtons(book)}
 
@@ -275,65 +332,63 @@ function Dashboard() {
   return (
     <div>
 
-      {loading ?
+      {loading &&
         <div className="preloader" id="preloader">
-          <img width="300" src={loader} alt="Loading ..." />
+          <img src={loader} alt="Loading ..." />
         </div>
-        :
-
-        (
-          <div>
-            <Header setSearchWord={setSearchWord} />
-            <div className="main-content">
-              <div className="title">
-                <div className="left-content">
-                  <span className="book">Books</span>
-                  <span className="items">({books.length} items)</span>
-                </div>
-                <div className="sort-options">
-                  <FormControl sx={{ m: 1, minWidth: 120 }}>
-                    <Select
-                      value={selection}
-                      onChange={handleChange}
-                      displayEmpty
-                      size="small"
-                    >
-                      <MenuItem style={{ display: "none" }} disabled value="">
-                        Sort by relevance
-                      </MenuItem>
-                      <MenuItem onClick={sortLowToHigh} style={{ fontSize: "15px" }} value={1}>Price: Low to High</MenuItem>
-                      <MenuItem onClick={sortHighToLow} style={{ fontSize: "15px" }} value={2}>Price: High to Low</MenuItem>
-                      <MenuItem onClick={sortAtoZ} style={{ fontSize: "15px" }} value={3}>Sort: A - Z</MenuItem>
-                      <MenuItem onClick={sortZtoA} style={{ fontSize: "15px" }} value={4}>Sort: Z - A</MenuItem>
-                    </Select>
-                  </FormControl>
-                </div>
-              </div>
-
-              <div>
-                {searchWord.length === 0 ? displayBooks(currentBooks) : displayBooks(currentBooks.filter(book => (book.bookName.toLowerCase().includes(searchWord) || book.author.toLowerCase().includes(searchWord))))}
-              </div>
-            </div>
-            <ThemeProvider theme={theme}>
-              <Box sx={{ display: 'flex', justifyContent: "center", margin: "25px" }} >
-                <Pagination
-                  onChange={handlePageChange}
-                  count={Math.ceil(books.length / booksPerPage)}
-                  color="myColor"
-                  page={currentPage}
-                  shape="rounded"
-                  renderItem={(item) => (
-                    <PaginationItem
-                      components={{ previous: left, next: right }}
-                      {...item}
-                    />
-                  )}
-                />
-              </Box>
-            </ThemeProvider>
-            <Footer />
-          </div>)
       }
+      <div>
+        <Header setSearchWord={setSearchWord} />
+        <div className="main-content">
+          <div className="title">
+            <div className="left-content">
+              <span className="book">Books</span>
+              <span className="items">({books.length} items)</span>
+            </div>
+            <div className="sort-options">
+              <FormControl sx={{ m: 1, minWidth: 120 }}>
+                <Select
+                  value={selection}
+                  onChange={handleChange}
+                  displayEmpty
+                  size="small"
+                >
+                  <MenuItem style={{ display: "none" }} disabled value="">
+                    Sort by relevance
+                  </MenuItem>
+                  <MenuItem onClick={sortLowToHigh} style={{ fontSize: "15px" }} value={1}>Price: Low to High</MenuItem>
+                  <MenuItem onClick={sortHighToLow} style={{ fontSize: "15px" }} value={2}>Price: High to Low</MenuItem>
+                  <MenuItem onClick={sortAtoZ} style={{ fontSize: "15px" }} value={3}>Sort: A - Z</MenuItem>
+                  <MenuItem onClick={sortZtoA} style={{ fontSize: "15px" }} value={4}>Sort: Z - A</MenuItem>
+                </Select>
+              </FormControl>
+            </div>
+          </div>
+
+          <div>
+            {searchWord.length === 0 ? displayBooks(currentBooks) : displayBooks(currentBooks.filter(book => (book.bookName.toLowerCase().includes(searchWord) || book.author.toLowerCase().includes(searchWord))))}
+          </div>
+        </div>
+        <ThemeProvider theme={theme}>
+          <Box sx={{ display: 'flex', justifyContent: "center", margin: "25px" }} >
+            <Pagination
+              onChange={handlePageChange}
+              count={Math.ceil(books.length / booksPerPage)}
+              color="myColor"
+              page={currentPage}
+              shape="rounded"
+              renderItem={(item) => (
+                <PaginationItem
+                  components={{ previous: left, next: right }}
+                  {...item}
+                />
+              )}
+            />
+          </Box>
+        </ThemeProvider>
+        <Footer />
+      </div>
+
     </div>
   )
 }
